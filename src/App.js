fe9e7b0db34a1aafc41b09b75fa3cf557669ea4d
Narottam04 <coutkunal@gmail.com>
2021-11-08 09:39:07 +0530

    created project and configured tailwind css

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className = "bg-blue-400">This is our lovely app</h1>
+    </div>
+  );
+}
+
+export default App;
