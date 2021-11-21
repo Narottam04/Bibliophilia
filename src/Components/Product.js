041c736c4b53706dda74b572223407a0ab3a826c
Narottam04 <coutkunal@gmail.com>
2021-11-21 07:52:18 +0530

    product size fixed on view all books page

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -23,7 +23,7 @@ function Product() {
                         return(
                             <Link key = {item.id} to ={`/products/${item.permalink}`} className="group relative">
                                 <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-                                <img src={item.image.url} alt = {item.name} className="w-full h-full object-center object-contain lg:w-full lg:h-full" />
+                                <img src={item.image.url} alt = {item.name} className="w-1/2 h-1/2 m-auto object-center object-contain lg:w-full lg:h-full" />
                                 </div>
                                 <div className="mt-4 flex justify-between">
                                 <div>
