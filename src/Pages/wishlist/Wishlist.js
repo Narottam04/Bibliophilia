d1b43b3d4a5a9618adce89d3f0c590f75808da70
Narottam04 <coutkunal@gmail.com>
2021-11-28 10:11:41 +0530

    typo change in whishlist and added helmet for meta info of page

--- src/Pages/wishlist/Wishlist.js
+++ src/Pages/wishlist/Wishlist.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import {commerce} from '../../lib/commerce'
+import { Helmet } from 'react-helmet'
 
 function Wishlist() {
     const [wishlist, setwishlist] = useState([])
@@ -26,6 +27,21 @@ function Wishlist() {
 
     return (
         <div>
+             <Helmet>
+                <title>Bibliophilia: Whislist</title>
+                <meta name="description" content="Here on Bibliophilia,we connect you, our book lovers, directly to the sellers. In this way, you can assure everything about the book and see for yourself by having a one-to-one interaction with the seller. So what are you waiting for, sign up and boost your reading journey right away!"/>
+                <link rel="canonical" href="https://bibliophilia.pages.dev/"/>
+                <meta property="og:title" content="A Platform Where You Can Buy And Sell Your Old Books."/>
+                <meta property="og:description" content="Here on Bibliophilia,we connect you, our book lovers, directly to the sellers. In this way, you can assure everything about the book and see for yourself by having a one-to-one interaction with the seller. So what are you waiting for, sign up and boost your reading journey right away!"/>
+                <meta property="og:image" content="/images/logo.png"/>
+                <meta property="og:image:width" content="2500"/>
+                <meta property="og:image:height" content="1330"/>
+                <meta property="og:site_name" content="Bibliophilia"/>
+                <meta property="og:type" content="ecommerce"/>
+                <meta name="language" content="EN"/>
+                <meta name="country" content="IN"/>
+                <meta name="author" content="Bibliophilia"/>
+            </Helmet>
             <div class="fixed inset-0 overflow-hidden" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
             <div class=" inset-0 overflow-hidden">
 
@@ -39,7 +55,7 @@ function Wishlist() {
                     <div class="flex-1 py-6 overflow-y-auto px-4 sm:px-6">
                         <div class="flex items-start justify-between">
                         <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">
-                            Shopping cart
+                            Your Wishlist
                         </h2>
                         <Link to = "/" class="ml-3 h-7 flex items-center">
                             <button type="button" class="-m-2 p-2 text-gray-400 hover:text-gray-500">
@@ -94,7 +110,7 @@ function Wishlist() {
                     <div class="border-t border-gray-200 py-6 px-4 sm:px-6">
                         <div class="mt-6 flex justify-center text-sm text-center text-gray-500">
                         <p>
-                            or <Link to = "/allbooks" type="button" class="text-indigo-600 font-medium hover:text-indigo-500">Continue Shopping<span aria-hidden="true"> &rarr;</span></Link>
+                            <Link to = "/allbooks" type="button" class="text-indigo-600 font-medium hover:text-indigo-500">Continue Shopping<span aria-hidden="true"> &rarr;</span></Link>
                         </p>
                         </div>
                     </div>
